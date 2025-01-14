e6b38dc12a8615a33afdeef03c7cfd0afae045a5
이서현 <mellifluous3380@gmail.com>
2025-01-14 20:28:12 +0900

    feat: 영화목록 마우스 호버 기능 추가

--- src/components/MovieListInfo/MovieListInfo.js
+++ src/components/MovieListInfo/MovieListInfo.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MovieListInfo.css';
 
 function MovieListInfo({ movie }) {
+    const [isHovering, setIsHovering] = useState(false);
+
     return (
-        <div className='movielist-container'>
+        <div
+            className='movielist-container'
+            onMouseOver={() => setIsHovering(true)}
+            onMouseOut={() => setIsHovering(false)}
+        >
+            {isHovering && (
+                <div className='movielist-hover'>
+                    <p>{movie.Title}</p>
+                    <p>{movie.Year}</p>
+                </div>
+            )}
             <img className='movielist-image' src={movie.Poster} />
             <p className='movielist-title'>{movie.Title}</p>
         </div>
