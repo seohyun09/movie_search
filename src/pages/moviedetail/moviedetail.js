996cd4b5d22a306db2982b42ad1b739cef82ea07
이서현 <mellifluous3380@gmail.com>
2025-01-15 20:25:43 +0900

    feat: API 요청 하는 동안 로딩화면 렌더링

--- src/pages/moviedetail/moviedetail.js
+++ src/pages/moviedetail/moviedetail.js
@@ -9,13 +9,16 @@ import { getMovieDetail } from '../../apis/MovieInfo';
 function MovieDetail() {
     const movieID = useParams().id;
     const [movieContent, setMovieContent] = useState();
+    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
         const fetchMovieDetail = async (movieID) => {
+            setIsLoading(true);
             if (movieID) {
                 const data = await getMovieDetail(movieID);
                 setMovieContent(data);
+                setIsLoading(false);
             }
         };
         fetchMovieDetail(movieID);
@@ -24,28 +27,37 @@ function MovieDetail() {
     return (
         <div className='moviedetail-box'>
             <MovieSearch />
-            <button className='moviedetail-button' onClick={() => navigate(-1)}>
-                {'<'}뒤로가기
-            </button>
-            <div className='moviedetail-container'>
-                <img className='moviedetail-image' src={movieContent?.Poster} />
-
-                <div className='moviedetail-contents'>
-                    <h1>{movieContent?.Title}</h1>
-                    <p>
-                        {movieContent?.Genre} / {movieContent?.Year}
-                    </p>
-                    <hr />
-                    <h3>상영시간</h3>
-                    <p>{movieContent?.Runtime}</p>
-                    <h3>감독</h3>
-                    <p>{movieContent?.Director}</p>
-                    <h3>배우</h3>
-                    <p>{movieContent?.Actors}</p>
-                    <h3>줄거리</h3>
-                    <p>{movieContent?.Plot}</p>
+            {isLoading && (
+                <div className='loading'>
+                    <p>Loading . . .</p>
                 </div>
-            </div>
+            )}
+            {!isLoading && (
+                <div>
+                    <button className='moviedetail-button' onClick={() => navigate(-1)}>
+                        {'<'}뒤로가기
+                    </button>
+                    <div className='moviedetail-container'>
+                        <img className='moviedetail-image' src={movieContent?.Poster} />
+
+                        <div className='moviedetail-contents'>
+                            <h1>{movieContent?.Title}</h1>
+                            <p>
+                                {movieContent?.Genre} / {movieContent?.Year}
+                            </p>
+                            <hr />
+                            <h3>상영시간</h3>
+                            <p>{movieContent?.Runtime}</p>
+                            <h3>감독</h3>
+                            <p>{movieContent?.Director}</p>
+                            <h3>배우</h3>
+                            <p>{movieContent?.Actors}</p>
+                            <h3>줄거리</h3>
+                            <p>{movieContent?.Plot}</p>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
